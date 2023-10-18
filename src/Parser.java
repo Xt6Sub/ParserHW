ff93a2106c4b5774691273d968b90fc7c8fe69df
Xt6Sub <127432333+Xt6Sub@users.noreply.github.com>
2023-10-18 00:42:39 -0400

    10/17 scanner v2....Finished. Parser class needs more commenting

--- src/Parser.java
+++ src/Parser.java
@@ -23,16 +23,25 @@ public class Parser{
     Scanner scanner;
 
     private void accept(byte expectedKind) {
-        if (currentToken.kind == expectedKind)
-            currentToken = scanner.scan();
+        if (currentToken.kind == expectedKind){
+            printToken();
+            currentToken = scanner.scan();}
         else
-            new Error("Syntax error: " + currentToken.spelling + " is not expected.",
+            new Error("Syntax error: " + currentToken.spelling + " is not expected."+" Expected: "+expectedKind,
                     currentToken.line);
     }
 
     private void acceptIt() {
         currentToken = scanner.scan();
+        printToken();
     }
+
+    //printing current token
+    private void printToken(){
+       //ie. Line: 1, spelling = [(], kind = 9
+        System.out.println("Line: "+currentToken.line+", spelling = ["+currentToken.spelling+"], kind = "+currentToken.kind);
+    }
+
 
     public void parse() {
         SourceFile sourceFile = new SourceFile();
@@ -46,64 +55,136 @@ public class Parser{
 
     //Program" --> "("Sequence State")".
     private void parseProgram() {
-
-    }
+        accept(Token.LPAREN);
+        parseSequence();
+        parseState();
+        accept(Token.RPAREN);
+          }
 
     //Sequence --> "("Statements")".
     private void parseSequence(){
-
-    }
+        accept(Token.LPAREN);
+        parseStatements();
+        accept(Token.RPAREN);
+            }
 
     //Statements --> Stmt*
     private void parseStatements(){
-
+        while(currentToken.kind != Token.RPAREN) {
+            parseStmt();
+        }
     }
 
     //Stmt --> "(" {NullStatement | Assignment | Conditional | Loop | Block}")".
     private void parseStmt(){
+        accept(Token.LPAREN);
 
+        switch (currentToken.kind) {
+            case (Token.SKIP):
+                parseNullStatement();
+                break;
+
+            case(Token.ASSIGN):
+                parseAssignment();
+                break;
+
+            case(Token.CONDITIONAL):
+                parseConditional();
+                break;
+
+            case(Token.LOOP):
+                parseLoop();
+                break;
+
+            case(Token.BLOCK):
+                parseBlock();
+                break;
+        }
+        accept(Token.RPAREN);
     }
 
     //State -->  "("Pairs")".
     private void parseState(){
-
+        accept(Token.LPAREN);
+        parsePairs();
+        accept(Token.RPAREN);
     }
 
     //Pairs --> Pair*.
     private void parsePairs(){
-
+        while(currentToken.kind != Token.RPAREN) {
+            parsePair();
+        }
     }
 
     //Pair --> "("Identifier Literal")".
     private void parsePair(){
+        accept(Token.LPAREN);
+        if(currentToken.kind==Token.IDENTIFIER){
+            acceptIt();
+        }
+        if (currentToken.kind==Token.LITERAL){
+            acceptIt();
+        }
+        accept(Token.RPAREN);
     }
 
     //NullStatement --> skip.
     private void parseNullStatement(){
+
+        accept(Token.SKIP);
+
     }
 
     //Assignment --> "assign" Identifier Expression.
     private void parseAssignment(){
+        accept(Token.ASSIGN);
+        if (currentToken.kind==Token.IDENTIFIER){
+            acceptIt();
+        }
+        parseExpression();
+
     }
 
     //Conditional --> "conditional" Expression Stmt Stmt.
     private void parseConditional(){
+        accept(Token.CONDITIONAL);
+        parseExpression();
+        parseStmt();
+        parseStmt();
     }
 
     //Loop --> "loop" Expression Stmt.
     private void parseLoop(){
+        accept(Token.LOOP);
+        parseExpression();
+        parseStmt();
     }
 
     //Block --> "block" Statements.
     private void parseBlock(){
+        accept(Token.BLOCK);
+        parseStatements();
     }
 
     //Expression --> Identifier | Literal | "("Operation Expression Expression")".
     private void parseExpression(){
+        if(currentToken.kind==Token.IDENTIFIER||currentToken.kind==Token.LITERAL){
+            acceptIt();
+        }
+        else{
+            accept(Token.LPAREN);
+            parseOperation();
+            parseExpression();
+            parseExpression();
+            accept(Token.RPAREN);}
     }
 
     //Operation --> "+" |"-" | "*" | "/" | "<" | "<=" | ">" | ">=" | "=" | "!=" | "or" | "and".
     private void parseOperation(){
+        if(currentToken.kind==Token.OPERATOR||currentToken.kind==Token.OR||currentToken.kind==Token.AND){
+            acceptIt();
+        }
     }
 }
 
